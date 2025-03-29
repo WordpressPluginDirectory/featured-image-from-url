b2a4c8fa2ba5d1fa0cb3cc57ab5c077e3c959ec0
Justin Gardner <justin@rhynorater.com>
2025-03-29 06:02:12 -0500

    5.0.5

--- featured-image-from-url/featured-image-from-url.php
+++ featured-image-from-url/featured-image-from-url.php
@@ -3,12 +3,12 @@
 /*
  * Plugin Name: Featured Image from URL (FIFU)
  * Plugin URI: https://fifu.app/
- * Description: Use an external image/video/audio as featured image of a post or WooCommerce product.
- * Version: 5.0.1
+ * Description: Use a remote image or video as featured image of a post or WooCommerce product.
+ * Version: 5.0.5
  * Author: fifu.app
  * Author URI: https://fifu.app/
  * WC requires at least: 4.0
- * WC tested up to: 9.7.0
+ * WC tested up to: 9.7.1
  * Text Domain: featured-image-from-url
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
